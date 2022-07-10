d7219efb54b41f6de595541803080d1a2ea888fe
boogiemonsta <boogiemonsta@gmail.com>
2022-07-10 17:45:00 +0200

    login UI WIP

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup; // circumvent TS strict class checking
+  isSubmitted = false;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      name: ['', Validators.required],
+    });
+  }
+
+  get formControls() {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authService.login(this.loginForm.value);
+    this.router.navigateByUrl('/gallery');
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor() {}
+  public login(input: User) {
+    localStorage.setItem('ACCESS_TOKEN', 'access_token');
+  }
+
+  public isLoggedIn() {
+    return localStorage.getItem('ACCESS_TOKEN') !== null;
+  }
+
+  public logOut() {
+    localStorage.removeItem('ACCESS_TOKEN');
+  }
+}
